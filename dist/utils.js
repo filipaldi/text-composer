0fa412b10a221000b37feec588a246f6bd2e5dca
Filip <flp@Filips-MacBook-Pro.local>
2024-07-09 13:20:42 +0200

    add `/dist` directory

---
+++ dist/utils.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.someUtilityFunction = void 0;
+function someUtilityFunction() {
+    // Utility function placeholder
+}
+exports.someUtilityFunction = someUtilityFunction;
